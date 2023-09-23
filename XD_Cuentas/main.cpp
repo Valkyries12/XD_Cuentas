f087adab5c5b3f6c4433ada92d9b4fff4726a898
Nicolás Caruso <nico.caruso1992@outlook.com>
2023-09-23 15:38:19 -0300

    Modificacion UsuarioModel
    Se agrega buscar por email

--- XD_Cuentas/main.cpp
+++ XD_Cuentas/main.cpp
@@ -12,5 +12,6 @@ int main()
     usuarioModel.crearArchivo();
     usuarioModel.registrar(usuarioModel.getUsuario());
     usuarioModel.leer();
+    cout << usuarioModel.buscar("nicolas");
     return 0;
 }
--- XD_Cuentas/src/UsuarioModel.cpp
+++ XD_Cuentas/src/UsuarioModel.cpp
@@ -74,9 +74,27 @@ bool UsuarioModel::leer() {
         while(fread(&usuario, sizeof(Usuario), 1, pFile)== 1) {
             std::cout << usuario.getEmail();
         }
+        fueLeido = true;
+        fclose(pFile);
     }
+
+    return fueLeido;
 }
 
 bool UsuarioModel::buscar(std::string email) {
+    Usuario usuario;
+    FILE* pFile;
+    bool fueEncontrado = false;
+    pFile = fopen(_nombreArchivo, "rb");
+    if (pFile != nullptr) {
+        while(fread(&usuario, sizeof(Usuario), 1, pFile) == 1) {
+            if (strcmp(usuario.getEmail().c_str(), email.c_str()) == 0) {
+                fueEncontrado = true;
+                break;
+            }
+        }
+        fclose(pFile);
+    }
 
+    return fueEncontrado;
 }
